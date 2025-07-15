0325d4d65a2026f91584d4f2633cbd055b6438f2
Nicholas Gabriel Rocha Ferreira <nigarofe@gmail.com>
2025-07-15 11:24:18 -0300

    fixed some ts compiling

--- _misc/changelog.md
+++ _misc/changelog.md
@@ -1,4 +1,6 @@
 2025-07-14 18:52
 General project folders and structure
 Installed npm dependencies
+
+2025-07-15
 Typescript compiling
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import { Request, Response } from 'express';
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = 3000;
+
 // Serve static files from the 'client' directory
 app.use(express.static(path.join(__dirname, '../client')));
+
 // Serve node_modules
 app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+app.use(express.static(path.join(__dirname, '../compiled')));
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
+
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
